c27fae74039d2ccc7f8ec9d92618193247514993
Alex Kempf <alekemp@atlasworldgroup.com>
2017-07-19 23:05:37 +0000

    App created.

---
+++ platform-hackfest/app/shared/shared.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { NativeScriptModule } from "nativescript-angular/platform";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { LISTVIEW_DIRECTIVES } from 'nativescript-telerik-ui/listview/angular';
+
+import * as common from "./";
+
+@NgModule({
+  imports: [
+    NativeScriptModule,
+    NativeScriptFormsModule
+  ],
+  declarations: [
+    /// additional declarations
+    common.ActionBarComponent,
+    common.IfAndroidDirective,
+    common.IfIosDirective,
+    common.HyperlinkDirective,
+    LISTVIEW_DIRECTIVES
+  ],
+  exports: [
+    /// additional exports
+    common.ActionBarComponent,
+    common.IfAndroidDirective,
+    common.IfIosDirective,
+    common.HyperlinkDirective,
+    LISTVIEW_DIRECTIVES
+  ],
+  providers: [
+    /// additional services
+    common.NavigationService,
+    common.NotificationService
+
+  ]
+})
+export class SharedModule {}
